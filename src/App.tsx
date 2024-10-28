6e9b850694957ac1406ad9cfda2f707f813f82a6
JiyeonMin <ichbinmin2@gmail.com>
2024-10-28 19:33:07 +0900

    section : 3-3,4,5

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import ThreeElement from "./ThreeElement";
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 
 function App() {
+  const color = useControls({
+    value: "white",
+  });
+
+  const grid = useControls({
+    segment: { value: 10, min: 2, max: 100, stpe: 1 },
+  });
+
   return (
     <>
       <Canvas
@@ -10,12 +20,29 @@ function App() {
         camera={{
           // zoom: 250,
           near: 1,
-          far: 20,
+          far: 100, // 얼마나 멀리 있는 것까지 카메라에서 렌더링을 할것인지를 결정하는 옵션값
           fov: 75, // orthographic에서는 필요 x
           position: [3, 3, 0],
         }}
       >
+        {/* Math.PI = 100
+        Math.PI * 2 = 360
+        Math.PI / 4 = 45  */}
+
+        <OrbitControls
+        // minAzimuthAngle={-Math.PI / 4} // -45
+        // maxAzimuthAngle={Math.PI / 4} // 45
+        // minPolarAngle={Math.PI / 6}
+        // maxPolarAngle={Math.PI - Math.PI / 6}
+        />
+        {/* 기본적으로 args 옵션 값은 미터단위로 계산된다. */}
+        <axesHelper args={[6]} />
+        <gridHelper args={[10, grid.segment]} />
         <ThreeElement />
+
+        {/* leva useControl */}
+
+        <color attach="background" args={[color.value]} />
       </Canvas>
     </>
   );
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,13 +1,15 @@
 import { useThree, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as Three from "three";
+import { useControls } from "leva";
 
 export default function ThreeElement() {
   const { size } = useThree();
-
   const boxRef = useRef<Three.Mesh>(null);
 
-  console.log(size);
+  const box = useControls({
+    rotation: { value: 0, min: -360, max: 360, stpe: 1 },
+  });
 
   // scene 에 대한 변화를 useFrame 내부에서 동작하도록 추가해야한다.
   useFrame((state, delta) => {
@@ -22,10 +24,12 @@ export default function ThreeElement() {
         ref={boxRef}
         rotation={[
           Three.MathUtils.degToRad(45), // x
-          Three.MathUtils.degToRad(45), // y
+          Three.MathUtils.degToRad(box.rotation), // y
           0, // z
         ]}
       >
+        {/* Geometry와 Material는 무조건 mesh 안에 넣어줘야 동작한다. */}
+        {/* <sphereGeometry /> */}
         <boxGeometry />
         <meshStandardMaterial color="blue" />
       </mesh>
