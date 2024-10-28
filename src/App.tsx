214adadbcf4343d3a4064f72257ed8c13f20e006
JiyeonMin <ichbinmin2@gmail.com>
2024-10-28 20:19:32 +0900

    section : 4-1,2

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,23 @@ import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import ThreeElement from "./ThreeElement";
 import { OrbitControls } from "@react-three/drei";
-import { useControls } from "leva";
 
 function App() {
-  const color = useControls({
-    value: "white",
-  });
-
-  const grid = useControls({
-    segment: { value: 10, min: 2, max: 100, stpe: 1 },
-  });
-
   return (
     <>
       <Canvas
-        // orthographic
         camera={{
-          // zoom: 250,
           near: 1,
-          far: 100, // 얼마나 멀리 있는 것까지 카메라에서 렌더링을 할것인지를 결정하는 옵션값
-          fov: 75, // orthographic에서는 필요 x
-          position: [3, 3, 0],
+          fov: 75,
+          far: 100,
+          position: [5, 5, 5],
         }}
       >
-        {/* Math.PI = 100
-        Math.PI * 2 = 360
-        Math.PI / 4 = 45  */}
-
-        <OrbitControls
-        // minAzimuthAngle={-Math.PI / 4} // -45
-        // maxAzimuthAngle={Math.PI / 4} // 45
-        // minPolarAngle={Math.PI / 6}
-        // maxPolarAngle={Math.PI - Math.PI / 6}
-        />
-        {/* 기본적으로 args 옵션 값은 미터단위로 계산된다. */}
+        <color attach="background" args={["white"]} />
+        <OrbitControls />
         <axesHelper args={[6]} />
-        <gridHelper args={[10, grid.segment]} />
+        <gridHelper args={[10, 10]} />
         <ThreeElement />
-
-        {/* leva useControl */}
-
-        <color attach="background" args={[color.value]} />
       </Canvas>
     </>
   );
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -4,35 +4,31 @@ import * as Three from "three";
 import { useControls } from "leva";
 
 export default function ThreeElement() {
-  const { size } = useThree();
+  const { size, scene } = useThree();
   const boxRef = useRef<Three.Mesh>(null);
-
-  const box = useControls({
-    rotation: { value: 0, min: -360, max: 360, stpe: 1 },
-  });
 
   // scene 에 대한 변화를 useFrame 내부에서 동작하도록 추가해야한다.
   useFrame((state, delta) => {
-    // boxRef.current.rotation.x += delta;
-    // boxRef.current.position.y -= 0.01;
-    // boxRef.current.scale.z += 0.01;
+    // scene.position.x += 0.01;
   });
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      <mesh
-        ref={boxRef}
-        rotation={[
-          Three.MathUtils.degToRad(45), // x
-          Three.MathUtils.degToRad(box.rotation), // y
-          0, // z
-        ]}
-      >
-        {/* Geometry와 Material는 무조건 mesh 안에 넣어줘야 동작한다. */}
-        {/* <sphereGeometry /> */}
-        <boxGeometry />
-        <meshStandardMaterial color="blue" />
-      </mesh>
+      <group position={[0, 0, 0]}>
+        <mesh
+          ref={boxRef}
+          position={[0, 0, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            Three.MathUtils.degToRad(0),
+            Three.MathUtils.degToRad(0),
+            Three.MathUtils.degToRad(0),
+          ]}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+      </group>
     </>
   );
 }
