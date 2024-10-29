97d9090779154caa1b4b44c0641970a6d2c291b9
JiyeonMin <ichbinmin2@gmail.com>
2024-10-29 20:21:43 +0900

    section : 5-1,2,3

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -2,74 +2,32 @@ import { useThree, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as Three from "three";
 import { useControls } from "leva";
+import { Box, Sphere, Cone } from "@react-three/drei";
 
 export default function ThreeElement() {
   const { size, scene } = useThree();
   const boxRef = useRef<Three.Mesh>(null);
-  const groupRef = useRef<Three.Group>(null);
-  // scene 에 대한 변화를 useFrame 내부에서 동작하도록 추가해야한다.
 
+  // scene 에 대한 변화를 useFrame 내부에서 동작하도록 추가해야한다.
   useFrame((state, delta) => {
     // scene.position.x += 0.01;
-    scene.rotation.x += 0.01;
-    groupRef.current.rotation.x += delta;
+    // scene.rotation.x += 0.01;
+    // groupRef.current.rotation.x += delta;
   });
 
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      <group
-        ref={groupRef}
-        position={[0, 0, 0]}
-        rotation={[
-          Three.MathUtils.degToRad(0),
-          Three.MathUtils.degToRad(45),
-          Three.MathUtils.degToRad(0),
-        ]}
-      >
-        <mesh
-          ref={boxRef}
-          position={[0, 0, 0]}
-          scale={[1, 1, 1]}
-          rotation={[
-            Three.MathUtils.degToRad(0),
-            Three.MathUtils.degToRad(0),
-            Three.MathUtils.degToRad(0),
-          ]}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="blue" />
-        </mesh>
-
-        <mesh
-          ref={boxRef}
-          position={[2, 0, 0]}
-          scale={[1, 1, 1]}
-          rotation={[
-            Three.MathUtils.degToRad(0),
-            Three.MathUtils.degToRad(45),
-            Three.MathUtils.degToRad(45),
-          ]}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="red" />
-          <axesHelper args={[3]} />
-        </mesh>
-
-        <mesh
-          ref={boxRef}
-          position={[0, 2, 0]}
-          scale={[1, 1, 1]}
-          rotation={[
-            Three.MathUtils.degToRad(0),
-            Three.MathUtils.degToRad(0),
-            Three.MathUtils.degToRad(0),
-          ]}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="green" />
-        </mesh>
-      </group>
+      <Sphere position={[-2, 0, 0]}>
+        <meshStandardMaterial color="green" />
+      </Sphere>
+      <mesh geometry={new Three.BoxGeometry(1, 1, 1)}>
+        <meshStandardMaterial color="red" />
+      </mesh>
+      <mesh ref={boxRef} position={[2, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="blue" />
+      </mesh>
     </>
   );
 }
